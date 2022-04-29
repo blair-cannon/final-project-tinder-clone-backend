7157394a1d7036837359d2f69176c30fd48ea41e
Blair Preston <blair.preston@student.awesomeinc.org>
2022-04-29 11:16:03 +0000

    create views that do not need filters

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,66 @@
-from django.shortcuts import render
+from .models import Location, Park, Breed, Gender, Socialization, Aggression, Tag, Size, User, Dog, Image, Connection, Conversation, Message, Comment
+from rest_framework import status, permissions, viewsets
+from .serializers import LocationSerializer, ParkSerializer, BreedSerializer, GenderSerializer, SocializationSerializer, AggressionSerializer, TagSerializer, SizeSerializer, UserSerializer, DogSerializer, ImageSerializer, ConnectionSerializer, ConversationSerializer, MessageSerializer, CommentSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter, OrderingFilter
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.decorators import action
 
-# Create your views here.
+
+class LocationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Locations to be viewed or edited.
+    """
+    queryset = Location.objects.all()
+    serializer_class = LocationSerializer
+
+class ParkViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Park.objects.all()
+    serializer_class = ParkSerializer
+
+class BreedViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Breed.objects.all()
+    serializer_class = BreedSerializer
+
+class GenderViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Gender.objects.all()
+    serializer_class = GenderSerializer
+
+class SocializationViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Socialization.objects.all()
+    serializer_class = SocializationSerializer
+
+class AggressionViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Aggression.objects.all()
+    serializer_class = AggressionSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+class SizeViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows Sizes to be viewed or edited.
+    """
+    queryset = Size.objects.all()
+    serializer_class = SizeSerializer
