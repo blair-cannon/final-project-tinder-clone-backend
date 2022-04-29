75ad898c8f5904904f91fa5741f4260a6628927d
Blair Preston <blair.preston@student.awesomeinc.org>
2022-04-29 11:17:58 +0000

    register models to django admin

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import *
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):    
+    model = User
+    list_display = ['username']
+
+# register models to Django admin
+admin.site.register(Location)
+admin.site.register(Park)
+admin.site.register(Breed)
+admin.site.register(Gender)
+admin.site.register(Socialization)
+admin.site.register(Aggression)
+admin.site.register(Tag)
+admin.site.register(Size)
+admin.site.register(Dog)
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Image)
+admin.site.register(Connection)
+admin.site.register(Conversation)
+admin.site.register(Message)
+admin.site.register(Comment)
